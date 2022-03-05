961b096406c52e192d209e536a65a83fdc84afd0
Connor White <connorwhitepdx@gmail.com>
2022-03-05 03:09:58 -0800

    404 error handling deleted

--- src/App.js
+++ src/App.js
@@ -8,17 +8,17 @@ import { BrowserRouter as Router,
 import 'react-bootstrap';
 
 // Page imports
+import Nav from './Nav';
 import Home from './Home';
 import About from './About';
 import Services from './Services';
 import Testimonials from './Testimonials';
-// import PageNotFound from './PageNotFound';
-import Nav from './Nav';
+
 
 function App() {
   return (
     <Router>
-      <div className="app">
+      <div className="app"> Hello!
         <Nav />
         <div className="content">
           <Routes>
