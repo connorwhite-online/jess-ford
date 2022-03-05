8f0de566cd29f41f12f5d599f1decae5611e7c76
Connor White <connorwhitepdx@gmail.com>
2022-03-05 00:49:46 -0800

    Routes imported for all pages

--- src/App.js
+++ src/App.js
@@ -3,11 +3,27 @@ import { BrowserRouter as Router, Route, Switch, Link, Redirect } from 'react-ro
 import 'react-bootstrap'
 import Nav from './Nav';
 
+// Page imports
+import index from './pages';
+import about from './pages';
+import services from './pages';
+import testimonials from './pages';
+import PageNotFound from './pages';
+
 function App() {
   return (
-    <>
-    <Nav />
-    </>
+    < Router>
+      <Switch>
+      <Route exact path='/' component={index}/>;
+      <Route exact path='/about' component={about}/>;
+      <Route exact path='/services' component={services}/>;
+      <Route exact path='/testimonials' component={testimonials}/>;
+      <Route exact path="/PageNotFound" component={PageNotFound}/>;
+      <Redirect to="/PageNotFound"/>
+      </Switch>
+
+    </Router>
+    
   )
 }
 
