b84d75bf7935fb3ab4a35aec432000817c42b5eb
Connor White <connorwhite@connors-imac.lan>
2022-03-08 00:23:59 -0800

    image loaded and centered, scale not working

--- src/views/About.js
+++ src/views/About.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import biopic from '../assets/images/image3.jpeg'
 
 export default function About() {
   return (
-    <div>about</div>
+    <div className="about">
+      <div className="bio-pic">
+        <img src={biopic} alt="Jess Ford" width="100%" height="auto"></img>
+      </div>
+    </div>
   )
 }
