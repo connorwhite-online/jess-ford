26aa09a78ff09be84d5769b8b141fcb79c421830
Connor White <connorwhitepdx@gmail.com>
2022-03-18 23:48:27 -0700

    serviceLink copy added

--- src/components/servicesMenu.js
+++ src/components/servicesMenu.js
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom'
 export default function servicesMenu() {
   return (
     <div className="servicesMenu">
-        <Link className="serviceLink" to="/services/fertility"></Link>
-        <Link className="serviceLink" to="/services/pregnancy"></Link>
-        <Link className="serviceLink" to="/services/birth-support"></Link>
-        <Link className="serviceLink" to="/services/perinatal-loss"></Link>
+        <Link className="serviceLink" to="/services/fertility">fertility</Link>
+        <Link className="serviceLink" to="/services/pregnancy">pregnancy</Link>
+        <Link className="serviceLink" to="/services/birth-support">birth support</Link>
+        <Link className="serviceLink" to="/services/perinatal-loss">perinatal loss</Link>
     </div>
   )
 }
