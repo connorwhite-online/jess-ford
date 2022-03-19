a81c054f9aeeff4358683c7260e390767fc137a5
Connor White <connorwhitepdx@gmail.com>
2022-03-18 23:58:10 -0700

    test route /services/fertility added to App

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ function App() {
             <Route path='about' element={<About />} />
             <Route path='/services' element={<Services />} />
             <Route path='/testimonials' element={<Testimonials />} />
+            <Route path='/services/fertility' element={<Fertility />} />
       </Routes>
       </div>
     </div>
